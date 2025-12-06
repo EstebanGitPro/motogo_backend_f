7e974d7f96868dff1f29c79311c5ea029b4c1481
EstebanGitPro <estebanpoly.e.a@gmail.com>
2025-12-06 09:33:37 -0500

    feat: prepare all SQL statements at repository initialization for message, person repositories, add logging and detected infrastucture errors.

--- platform/databases/repositories/message/repository.go
+++ platform/databases/repositories/message/repository.go
@@ -7,6 +7,7 @@ import (
 	"github.com/EstebanGitPro/motogo-backend/core/ports/output"
 	cachetypes "github.com/EstebanGitPro/motogo-backend/platform/cache/types"
 	"github.com/EstebanGitPro/motogo-backend/platform/databases/common"
+	"github.com/EstebanGitPro/motogo-backend/platform/logger"
 )
 
 const (
@@ -56,10 +57,20 @@ const (
 		LIMIT 1`
 )
 
+var log logger.Logger = logger.NewSlogLogger()
 
 // repository implements output.MessageRepository
 type repository struct {
-	db *sql.DB
+	stmtGetAllActive               *sql.Stmt
+	stmtGetByCode                  *sql.Stmt
+	stmtGetByCodeForCache          *sql.Stmt
+	stmtGetByID                    *sql.Stmt
+	stmtGetByType                  *sql.Stmt
+	stmtGetByModule                *sql.Stmt
+	stmtMessageSave                *sql.Stmt
+	stmtMessageDelete              *sql.Stmt
+	stmtGetByCodeIncludingInactive *sql.Stmt
+	db                             *sql.DB
 }
 
 type MessageRepository interface {
@@ -72,7 +83,74 @@ func NewMessageRepository(db *sql.DB) (MessageRepository, error) {
 	if db == nil {
 		return nil, sql.ErrConnDone
 	}
-	return &repository{db: db}, nil
+
+	// Prepare all statements to fail-fast on application startup if SQL queries are malformed
+	stmtGetAllActive, err := db.Prepare(queryGetAllActive)
+	if err != nil {
+		log.Error(logger.LogDatabaseUnavailable, "error preparing stmtGetAllActive", err)
+		return nil, err
+	}
+
+	stmtGetByCode, err := db.Prepare(queryGetByCode)
+	if err != nil {
+		log.Error(logger.LogDatabaseUnavailable, "error preparing stmtGetByCode", err)
+		return nil, err
+	}
+
+	stmtGetByCodeForCache, err := db.Prepare(queryGetByCodeForCache)
+	if err != nil {
+		log.Error(logger.LogDatabaseUnavailable, "error preparing stmtGetByCodeForCache", err)
+		return nil, err
+	}
+
+	stmtGetByID, err := db.Prepare(queryGetByID)
+	if err != nil {
+		log.Error(logger.LogDatabaseUnavailable, "error preparing stmtGetByID", err)
+		return nil, err
+	}
+
+	stmtGetByType, err := db.Prepare(queryGetByType)
+	if err != nil {
+		log.Error(logger.LogDatabaseUnavailable, "error preparing stmtGetByType", err)
+		return nil, err
+	}
+
+	stmtGetByModule, err := db.Prepare(queryGetByModule)
+	if err != nil {
+		log.Error(logger.LogDatabaseUnavailable, "error preparing stmtGetByModule", err)
+		return nil, err
+	}
+
+	stmtMessageSave, err := db.Prepare(queryMessageSave)
+	if err != nil {
+		log.Error(logger.LogDatabaseUnavailable, "error preparing stmtMessageSave", err)
+		return nil, err
+	}
+
+	stmtMessageDelete, err := db.Prepare(queryMessageDelete)
+	if err != nil {
+		log.Error(logger.LogDatabaseUnavailable, "error preparing stmtMessageDelete", err)
+		return nil, err
+	}
+
+	stmtGetByCodeIncludingInactive, err := db.Prepare(queryGetByCodeIncludingInactive)
+	if err != nil {
+		log.Error(logger.LogDatabaseUnavailable, "error preparing stmtGetByCodeIncludingInactive", err)
+		return nil, err
+	}
+
+	return &repository{
+		db:                             db,
+		stmtGetAllActive:               stmtGetAllActive,
+		stmtGetByCode:                  stmtGetByCode,
+		stmtGetByCodeForCache:          stmtGetByCodeForCache,
+		stmtGetByID:                    stmtGetByID,
+		stmtGetByType:                  stmtGetByType,
+		stmtGetByModule:                stmtGetByModule,
+		stmtMessageSave:                stmtMessageSave,
+		stmtMessageDelete:              stmtMessageDelete,
+		stmtGetByCodeIncludingInactive: stmtGetByCodeIncludingInactive,
+	}, nil
 }
 
 func (r *repository) BeginTx(ctx context.Context) (output.Tx, error) {
