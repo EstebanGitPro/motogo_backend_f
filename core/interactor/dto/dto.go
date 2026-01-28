639235f3ad11110baf1f55daedf14b50dc5497fb
EstebanGitPro <estebanpoly.e.a@gmail.com>
2026-01-28 11:25:11 -0500

    refactor: remove comments from dto

--- core/interactor/dto/dto.go
+++ core/interactor/dto/dto.go
@@ -4,13 +4,11 @@ import (
 	"github.com/EstebanGitPro/motogo-backend/core/interactor/services/domain"
 )
 
-// RegistrationResult contiene el resultado del registro de usuario
 type RegistrationResult struct {
 	Person  domain.Person `json:"person"`
 	Message string        `json:"message"`
 }
 
-// UserSyncStatus representa el estado de sincronización con Keycloak
 type UserSyncStatus struct {
 	PersonID       string `json:"person_id"`
 	KeycloakUserID string `json:"keycloak_user_id"`
